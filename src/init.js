40e53e085a467faaa3a2516536f878eed8555633
Ryan Williams <ryan@wigg.ly>
2011-12-10 00:07:19 +1300

    Add missing requestAnimationFrame

--- src/init.js
+++ src/init.js
@@ -54,3 +54,19 @@ if (!Function.prototype.bind) {
 
     };
 }
+
+if (!window.requestAnimationFrame) {
+    /**
+     * Provides requestAnimationFrame in a cross browser way.
+     * @see http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+     */
+    window.requestAnimationFrame = ( function() {
+        return window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+            window.setTimeout( callback, 1000 / 60 );
+        };
+    } )();
+}
